ea99c09d9e5ce6157eeb5d6fd01dc5c2cc9c5023
saltobujang <saltobujang@gmail.com>
2024-12-24 02:49:07 +0700

    server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ function parseEnvList(env) {
 }
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+// var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
